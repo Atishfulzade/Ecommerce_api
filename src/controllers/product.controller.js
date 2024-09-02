de88376ada9d8d901296ff9b0ce7aa91848e9d64
Atishfulzade <atishfulzade1234@gmail.com>
2024-09-02 22:49:19 +0530

    s

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -74,7 +74,7 @@ export const filterProduct = async (req, res) => {
 // Find product by ID
 export const findById = async (req, res) => {
   try {
-    const { id } = req.params; // Correct destructuring
+    const { id } = req.params;
     const product = await Product.findById(id);
     if (!product) {
       return res.status(404).json({ message: "Product not found" });
